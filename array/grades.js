0679756a302d2d4b551c8a0750ac4746696c8c8f
Aldo Caruso <aldocaruso1991@gmail.com>
2020-03-15 11:22:16 +0100

    update

--- array/grades.js
+++ array/grades.js
@@ -1,1 +1,24 @@
+function main(){
+    this.grades = []
+    this.add = add
+    this.avg = avg
+}
 
+function add(grade) {
+    this.grades.push(grade)
+}
+
+function avg(){
+    var total = 0
+    return this.grades.reduce((acc, curr) => acc + curr) / this.grades.length
+
+}
+
+var gradeList = new main()
+gradeList.add(8)
+gradeList.add(6)
+gradeList.add(5)
+gradeList.add(6)
+gradeList.add(2)
+
+console.log(gradeList.avg())
