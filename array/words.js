b8d6f457b4b5ab96dd07dc2c8c5a2e2c7930a51c
Aldo Caruso <aldocaruso1991@gmail.com>
2020-03-15 12:17:31 +0100

    update

--- array/words.js
+++ array/words.js
@@ -1,1 +1,20 @@
+let wordsObj = {
+    words: [],
+    add(word) {
+        this.words.push(word)
+    },
+    wordForward(){
+        return this.words.join(", ")
+    },
+    wordBackward(){
+        return this.words.reduce((acc, curr) => curr + ", " + acc)
+    }
+}
 
+wordsObj.add("primavera")
+wordsObj.add("estate")
+wordsObj.add("autunno")
+wordsObj.add("inverno")
+
+console.log(wordsObj.wordForward())
+console.log(wordsObj.wordBackward())
