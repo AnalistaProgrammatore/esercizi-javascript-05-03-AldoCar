e0927efbaf50aab62d5cfb99a15e685116608845
Aldo Caruso <aldocaruso1991@gmail.com>
2020-03-16 12:02:12 +0100

    updare

--- array/letters.js
+++ array/letters.js
@@ -1,1 +1,19 @@
 
+var randomLetter = []
+
+function add(letter){
+    randomLetter.push(letter)
+    
+}
+
+add("a")
+add("l")
+add("d")
+add("o")
+
+function convertStr(randomLetter, item) {
+    return randomLetter + item
+}
+
+var converted = randomLetter.reduce(convertStr)
+console.log(converted)
