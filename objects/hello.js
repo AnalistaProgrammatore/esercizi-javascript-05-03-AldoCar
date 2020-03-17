1f35b31c9219791106ef6746c77658db54deedd3
Aldo Caruso <aldocaruso1991@gmail.com>
2020-03-17 11:19:57 +0100

    Update

--- objects/hello.js
+++ objects/hello.js
@@ -1,1 +1,20 @@
+let user = {}
 
+user.name = "Mario"
+user.surname= "Rossi"
+
+Object.defineProperties(
+    user,{
+    "surname": {
+        writeable: false,
+        configurable: false
+        }
+    }
+)
+
+console.log(Object.getOwnPropertyDescriptors(user))
+console.log(user)
+
+user.name = "Marco"
+console.log(user)
+delete user.name
