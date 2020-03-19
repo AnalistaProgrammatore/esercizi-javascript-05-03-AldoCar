5e11e26d08462a5453cdc3750b568a40ebce3b9e
Aldo Caruso <aldocaruso1991@gmail.com>
2020-03-19 10:44:13 +0100

    update

--- objects/salaries.js
+++ objects/salaries.js
@@ -1,1 +1,15 @@
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
 
+function sumSalaries(){
+    let total = 0
+    for (obj in salaries){
+        total = total + salaries[obj]
+    }
+    return total
+}
+
+console.log(sumSalaries(salaries))
