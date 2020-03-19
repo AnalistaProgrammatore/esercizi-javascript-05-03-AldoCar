19f029a9af8ad0c5024a80c24e59bf469d3c1c17
Aldo Caruso <aldocaruso1991@gmail.com>
2020-03-19 12:12:14 +0100

    Update da fixare

--- objects/multiply.js
+++ objects/multiply.js
@@ -1,1 +1,18 @@
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+}
 
+const multiplyNumeric = function (menu, multiplier) {
+    var newMenu = {}
+    for (let val in menu){
+        if (typeof menu[val]=== "number"){
+            menu[val] * multiplier
+        }            
+    }
+    return newMenu
+}
+
+const newMenu = multiplyNumeric(menu, 2)
+console.log(newMenu)
