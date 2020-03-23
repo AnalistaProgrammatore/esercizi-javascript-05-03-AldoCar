1109fcaa27cb868e5c6d08bfec52059a9e352f10
Aldo Caruso <aldocaruso1991@gmail.com>
2020-03-23 12:37:38 +0100

    update

--- objects/chaining.js
+++ objects/chaining.js
@@ -1,1 +1,18 @@
-
+class Chaining{
+    costructor(){
+       this.step = 0
+    }
+    up() {
+        this.step++
+       return this
+    }
+    down() {
+      this.step--
+      return this
+    }
+    showStep() { // shows the current step
+      console.log( this.step )
+    }
+}
+const newLadder = new Chaining()
+newLadder.up().up().down().showStep()
