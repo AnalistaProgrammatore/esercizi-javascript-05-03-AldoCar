914b59786f531d61e9b331df5956e15ff8173aa2
Aldo Caruso <aldocaruso1991@gmail.com>
2020-03-23 12:48:27 +0100

    Update

--- objects/calculatorConstructor.js
+++ objects/calculatorConstructor.js
@@ -1,1 +1,27 @@
-
+class Calculator{
+    costructor(){
+    values = []
+    }
+   sum(){
+       return this.values.reduce((a, b) => a + b)
+   }
+   sub() {
+       return this.values.reduce((a, b) => a - b)
+   }
+   mul() {
+       return this.values.reduce((a, b) => a * b)
+   }
+   div() {
+       if (this.values.includes(0)){
+           return console.log("Ops c'è un problema, non puoi dividere per 0")
+       } else return this.values.reduce((a, b) => a / b)
+       
+   }
+ }
+ 
+const newCalculator = new Calculator
+newCalculator.values = [9, 2];
+console.log( newCalculator.sum() )
+console.log( newCalculator.sub() )
+console.log( newCalculator.mul() )
+console.log( newCalculator.div() )
